3402a620dba7c02a7837634d91f8d7f5af7fb657
f4faysal <f4faysals@gmail.com>
2023-03-24 23:26:50 +0600

    add a tabil

--- voting-client-side/src/Layout/DashboardLayout.js
+++ voting-client-side/src/Layout/DashboardLayout.js
@@ -9,7 +9,7 @@ const DashboardLayout = () => {
 
       <div className="drawer drawer-mobile">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <div className="drawer-content ">
           {/* <!-- Page content here --> */}
           <Outlet></Outlet>
         </div>
