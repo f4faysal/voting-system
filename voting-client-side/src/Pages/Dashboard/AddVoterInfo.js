b0310d3f4bbec881c1d502273f676bdb3141c08a
f4faysal <f4faysals@gmail.com>
2023-03-26 04:59:57 +0600

    table data fatch

--- voting-client-side/src/Pages/Dashboard/AddVoterInfo.js
+++ voting-client-side/src/Pages/Dashboard/AddVoterInfo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import AddServiceForm from '../../Components/Form/AddServiceForm';
-import SearchForm from '../../Components/Form/SearchForm';
 
 const AddVoterInfo = () => {
     return (
         <div>
             <AddServiceForm></AddServiceForm>
-            <SearchForm></SearchForm>
+            
         </div>
     );
 };
